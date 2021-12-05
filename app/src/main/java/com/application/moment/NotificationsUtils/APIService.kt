2957e057f6d0c631f4686176785c6cd782e39a3f
Riad <58702474+riad73@users.noreply.github.com>
2021-12-05 14:42:14 +0100

    Update APIService.kt

--- app/src/main/java/com/application/moment/NotificationsUtils/APIService.kt
+++ app/src/main/java/com/application/moment/NotificationsUtils/APIService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.POST
 interface APIService {
     @Headers(
         "Content-Type:application/json",
-        "Authorization:key=AAAABCy4Xl0:APA91bGF645BnYpfLIZpGxKcMX7jdquQdbQD4j_yoe1KheM2e5RjQLGXZem4vPtGlnb0mvNfwhYFxVwhti7LoXw2gYaM67zRaF0hSidgr4SPlM8cE8IWsCoLX-JhOsFuV3TfmUnQPyG9"
+        "Authorization:key=YOUR_FIREBASE_NOTIFICATION KEY"
     )
     @POST("fcm/send")
     fun sendNotification(@Body body: NotificationSender?): Call<MyResponse?>
